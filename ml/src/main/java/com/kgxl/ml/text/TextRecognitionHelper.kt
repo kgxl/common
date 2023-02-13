72a68bc244f128a0ab87c73b98f138dacaf4c1fa
zjy <jinyu.zheng@tenclass.com>
2023-02-13 17:06:27 +0800

    [Update] 1.1.0.3

--- ml/src/main/java/com/kgxl/ml/text/TextRecognitionHelper.kt
+++ ml/src/main/java/com/kgxl/ml/text/TextRecognitionHelper.kt
@@ -1,10 +1,7 @@
 package com.kgxl.ml.text
 
 import android.media.Image
-import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 
 /**
@@ -12,14 +9,6 @@ import com.google.mlkit.vision.text.latin.TextRecognizerOptions
  */
 object TextRecognitionHelper {
     fun recognition(image: Image, invoke: (Text) -> Unit) {
-        val image = InputImage.fromMediaImage(image, 0)
-        val recognizer = TextRecognition.getClient(TextRecognizerOptions())
-        recognizer.process(image)
-            .addOnSuccessListener { texts ->
-                invoke.invoke(texts)
-            }
-            .addOnFailureListener { e -> // Task failed with an exception
-                e.printStackTrace()
-            }
+
     }
 }
