7416a24fe2610c4f63c942ea153be40be637699d
zjy <jinyu.zheng@tenclass.com>
2024-04-22 11:24:56 +0800

    [Update] 1.1.0.10 新增app扩展函数

--- base/src/main/java/com/kgxl/base/ext/ContextExt.kt
+++ base/src/main/java/com/kgxl/base/ext/ContextExt.kt
@@ -34,3 +34,56 @@ fun Context?.isFinishing(): Boolean {
     }
     return this.isFinishing
 }
+
+
+fun Context.getVersionCode(): String {
+    return getVersionCode(this).toString()
+}
+
+private fun getVersionCode(context: Context): Long {
+    val pi = context.packageManager.getPackageInfo(context.packageName, 0)
+    return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+        pi?.longVersionCode ?: -1
+    } else {
+        (pi?.versionCode ?: -1).toLong()
+    }
+}
+
+fun Context.toMarket(isGoogle: Boolean = true) {
+    val intent = Intent(Intent.ACTION_VIEW)
+    if (isGoogle) {
+        intent.data =
+            Uri.parse("https://play.google.com/store/apps/details?id=$packageName") //google
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    } else {
+        intent.data =
+            Uri.parse("market://details?id=$packageName") //跳转到应用市场，非Google Play市场一般情况也实现了这个接口
+        if (intent.resolveActivity(packageManager) != null) {
+            //可以接收
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, R.string.no_store, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+fun Context.toShare() {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        putExtra(
+            Intent.EXTRA_TEXT,
+            "${getString(R.string.share_sudoku)} https://play.google.com/store/apps/details?id=${packageName}"
+        )
+        type = "text/plain"
+    }
+    Intent.createChooser(intent, null)
+    startActivity(intent)
+}
+
+fun Context.toBrowse(url: String) {
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        addCategory(Intent.CATEGORY_BROWSABLE)
+        data = Uri.parse(url)
+    }
+    startActivity(intent)
+}
